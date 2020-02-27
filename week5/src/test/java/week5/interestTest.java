ee093c357bf432eecc1fd5507be80e6155f77399
ysikh <ysikh@DESKTOP-PU8PI84>
2020-02-27 16:49:40 +0530

    commit

--- week5/src/test/java/week5/interestTest.java
+++ week5/src/test/java/week5/interestTest.java
@@ -5,18 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class interestTest {
-	 private static final double DELTA = 1e-15;
-	    interest t=new interest();
-	    
-	  @Test
-	  public void testSimple() {
-	    assertEquals(60.0,t.simple(1000.0,2.0,3.0),DELTA);
-	  }
+    interest t=new interest();
+	@Test
+	public void testSimple() {
+		assertEquals(1.0,t.simple(10,10,1),0);
+	}
 
-	  @Test
-	  
-	  public void testCompound() {
-	    assertEquals(1333.099243,t.compound(1200.0,5.4,2.0),DELTA);
-	  }
+	@Test
+	public void testCompound() {
+		assertEquals(20.0,t.compound(10.0, 100.0, 1.0),0);
+	}
 
 }
